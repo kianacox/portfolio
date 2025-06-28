2cdb688ed6a00d7b9abd85ff69367a24cc9947d5
kco04 <kiana.cox@flutteruki.com>
2025-06-28 21:32:34 +0100

    observability: added vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from './page.module.css';
 import Content from './components/Content';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <main className={styles.main}>
         <Content />
         <SpeedInsights />
+        <Analytics />
       </main>
     </div>
   );
