ce3c61d16ff45690ee27315f75fe770d14cd7cae
kco04 <kiana.cox@flutteruki.com>
2025-06-28 14:10:06 +0100

    chore: added jest and husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from './page.module.css';
 import Content from './components/Content';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Content />
+        <SpeedInsights />
       </main>
     </div>
   );
